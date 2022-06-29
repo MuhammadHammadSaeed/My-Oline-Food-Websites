7a10ce0b2033ef49367c440578027c5603bb3d47
Hammad Saeed <Hammad Saeed@DESKTOP-U62S3LS>
2022-06-29 07:05:13 +0500

    Add project files.

---
+++ My online Food websites/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace My_online_Food_websites.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
